462aa215a295181c17c3dc3c00b5b883a6e7a0eb
Tim McEwan <tim@mcewan.it>
2014-02-09 12:26:05 +1100

    no persistence, no totals, no routes

--- app.rb
+++ app.rb
@@ -1,26 +1,13 @@
 require 'sinatra'
 require 'data_mapper'
 
-DataMapper::Logger.new($stdout, :debug)
-DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/coffeecalc.db")
-
-class Coffee
-  include DataMapper::Resource
-  property :id, Serial
-  property :what, String, required: true
-  property :cost, Decimal, required: true, default: 0.0
-  property :who, String
-end
-
-DataMapper.finalize.auto_upgrade!
+@@coffees ||= []
 
 get '/' do
-  Coffee.create!(params) if params[:what]
-  @coffees = Coffee.all
-  @total = total(@coffees)
   erb :index
 end
 
-def total(coffees)
-  coffees.map(&:cost).reduce(:+)
+post '/' do
+  @@coffees << params
+  redirect '/'
 end
