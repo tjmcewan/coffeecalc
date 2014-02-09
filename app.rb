7a1dedb5d5d0ac4cbe2b23f0eee761e8e63e74dd
Tim McEwan <tim@mcewan.it>
2014-02-09 12:41:18 +1100

    simpler

--- app.rb
+++ app.rb
@@ -4,10 +4,30 @@ require 'data_mapper'
 @@coffees ||= []
 
 get '/' do
-  erb :index
+  """
+  <html>
+  <head><title>coffee calc</title></head>
+  <body>
+    <form action='/' method='post'>
+      What: <input name='what'>
+      Cost: <input name='cost'>
+      Who: <input name='who'>
+      <button type='submit'>add coffee</button>
+    </form>
+    #{coffees}
+  </body>
+  </html>
+  """
 end
 
 post '/' do
   @@coffees << params
   redirect '/'
+end
+
+
+def coffees
+  @@coffees.map { |coffee|
+    "<div>" + coffee.values.join(' - ') + "</div>"
+  }.join('<br>')
 end
